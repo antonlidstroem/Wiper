3e79243d248b11bd37e09b562af6c50aa5918e28
Anton Lidstroem <antonlidstroem@outlook.com>
2026-03-14 20:19:00 +0100

    Add project files.

---
+++ Wiper.wpf/Models/ProjectFolder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wiper.wpf.Models
+{
+    public class ProjectFolder
+    {
+        public string Name { get; init; } = string.Empty;
+        public string FullPath { get; init; } = string.Empty;
+        public bool IsSelected { get; set; } = true;
+    }
+}
